541c9062f1b7f54d0a13f5073df023b5605a6a5c
naman <namanshukla256@gmail.com>
2025-01-03 14:36:41 +0530

    3/1

--- JSFOUNDATION/part5/functions.js
+++ JSFOUNDATION/part5/functions.js
@@ -23,7 +23,6 @@ Store the result in a variable named `totalCost`.
 */
 
 
-
 /* 
 4. Write a function named `processTeaOrder` that takes another function, `makeTea`, as a parameter and calls it with the argument `"earl grey"`. 
 Return the result of calling `makeTea`.
@@ -31,8 +30,8 @@ Return the result of calling `makeTea`.
 
 
 
+
 /* 
 5. Write a function named `createTeaMaker` that returns another function. The returned function should take one parameter, `teaType`, and return a message like `"Making green tea"`. 
 Store the returned function in a variable named `teaMaker` and call it with `"green tea"`.
 */
-
